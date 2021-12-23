fe10b6a0b0db22db8a529a314b9b3893881771a2
Felix Lange <mail@felixlange.dev>
2021-12-23 10:17:37 +0100

    initial cinemaschine implementation

---
+++ CinePath/Cart.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Cinemachine;
+using Path;
+
+namespace CinePath
+{
+    public class Cart : PathingObjBase
+    {
+        [SerializeField] private CinemachineDollyCart cart;
+        [SerializeField] private Transform offsetAnchor;
+
+        private CartPath path;
+
+        public override bool Paused
+        {
+            get => cart.gameObject.activeInHierarchy;
+            set => cart.gameObject.SetActive(value);
+        }
+
+        private void OnEnable()
+        {
+            cart.m_PositionUnits = CinemachinePathBase.PositionUnits.Normalized;
+            cart.m_Speed = 0;
+        }
+
+        public override void SetPosition(float t, Vector3 offset, bool backwards = false)
+        {
+            cart.m_Position = t;
+            offsetAnchor.localPosition = offset;
+            if (backwards)
+            {
+                transform.localRotation = Quaternion.LookRotation(Vector3.back);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.identity;
+            }
+        }
+
+        public override void SetPath(IPath newPath)
+        {
+            if (!ReferenceEquals(path, newPath))
+            {
+                path = newPath as CartPath;
+                cart.m_Path = path.Path;
+            }
+        }
+    }
+}
